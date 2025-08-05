a90f88859891aa224738d043eea0420c4d9607a2
fuadinadhif <nadhiffuadi@gmail.com>
2025-08-05 08:01:34 +0700

    add cross-env

--- packages/schemas/src/user.schema.js
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-export const CreateUserSchema = z.object({
-    name: z.string().min(3, "Name must be at least 3 characters"),
-    email: z.email("Invalid email format"),
-});
-export const UpdateUserSchema = z.object();
